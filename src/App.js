cd23039f24a4d6448f8a66e4b5564bd29c133e22
Victor Adewumi <VictorOluwaseun@users.noreply.github.com>
2020-10-11 20:48:44 +0100

    Added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <h1>
+        <Header/>
         facebook-clone
       </h1>
     </div>
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = () => <header>Head</header>;
+
+export default Header;
